bab7c69c6d41baeb7b99ddbd477535b330f7a668
PJ <jaegersen@gmail.com>
2020-10-19 17:21:33 -0700

    fixed all static folder to pass through

--- .eleventy.js
+++ .eleventy.js
@@ -77,8 +77,7 @@ module.exports = function(eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
-  eleventyConfig.addPassthroughCopy("static/css");
+  eleventyConfig.addPassthroughCopy("static/");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
 
